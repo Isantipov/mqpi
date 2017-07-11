b0c61aaf8abbf4f16bece72826cc32ebdf0b7602
Ivan Antsipau <ivan.antsipau@adform.com>
2017-07-12 00:27:18 +0300

    post and put dummy impl

--- mqpi/Controllers/ValuesController.cs
+++ mqpi/Controllers/ValuesController.cs
@@ -3,36 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Rest;
 
 namespace mqpi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/{type}")]
     public class ValuesController : Controller
     {
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<string> Get(string type)
         {
-            return new string[] { "value1", "value2" };
+            return new string[]{"smth", "smth2"};
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public string Get(string type, int id)
         {
-            return "value";
+            return "type";
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public object Post(string type, [FromBody]dynamic value)
         {
+            var random = new Random(DateTime.UtcNow.Second);
+            value.Id = random.Next();
+
+            return value;
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public dynamic Put(string type, [FromBody]dynamic value, int id)
         {
+            if (value.id != id)
+            {
+                return new BadRequestObjectResult("Id in the object must match id in URI");
+            }
+
+            return value;
         }
 
         // DELETE api/values/5
