9237cdea4081691676f1b7d865dc2a9a2ee773f5
Ivan Antsipau <ivan.antsipau@adform.com>
2017-07-13 23:27:25 +0300

    add log4net

--- mqpi/Program.cs
+++ mqpi/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using System.Xml;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -12,9 +14,22 @@ namespace mqpi
 {
     public class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(Program));
         public static void Main(string[] args)
         {
             Console.WriteLine("hello");
+
+            // ReSharper disable once InconsistentNaming
+            var log4netConfig = new XmlDocument();
+            log4netConfig.Load(File.OpenRead("log4net.config"));
+
+            var repo = log4net.LogManager.CreateRepository(
+                Assembly.GetEntryAssembly(), typeof(log4net.Repository.Hierarchy.Hierarchy));
+
+            log4net.Config.XmlConfigurator.Configure(repo, log4netConfig["log4net"]);
+
+            log.Info("Application - Main is invoked");
+
             BuildWebHost(args).Run();
         }
 
