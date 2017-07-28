0d75c021c628b94fc6c5ec03f47c84c1f715fc21
Ivan Antsipau <ivan.antsipau@adform.com>
2017-07-28 19:21:05 +0300

    add GET _sys/Traces

---
+++ mqpi/Controllers/TracesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using IOFile = System.IO.File;
+
+namespace mqpi.Controllers
+{
+    [Route("_sys/Traces")]
+    public class TracesController : Controller
+    {
+        public string Get(int offset = 0, int limit = 100)
+        {
+            
+            var lines = ReadLines()
+                .Skip(offset).Take(limit);
+            var result = string.Join(Environment.NewLine, lines);
+            
+            return result;
+        }
+
+        private static IEnumerable<string> ReadLines()
+        {
+            using (FileStream file = IOFile.Open("../Logs/mqpi4.log", FileMode.OpenOrCreate, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (var textReader = new StreamReader(file))
+                {
+                    while (!textReader.EndOfStream)
+                    {
+                        yield return textReader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
