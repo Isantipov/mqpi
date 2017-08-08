5a50c0fe7a1d8337301306dda01b0ca11ca20d48
Ivan Antsipau <ivan.antsipau@adform.com>
2017-08-08 23:05:50 +0300

    trace headers

--- mqpi/Startup.cs
+++ mqpi/Startup.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -13,6 +16,7 @@ namespace mqpi
 {
     public class  Startup
     {
+        private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(typeof(Startup));
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -22,7 +26,32 @@ namespace mqpi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.Use(async (context, next) =>
+            {
+                // Do work that doesn't write to the Response.
+                LogRequest(context.Request);
+                await next.Invoke();
+                // Do logging or other work that doesn't write to the Response.
+                //Log.Info($"middleware. Request: {context.Request}");
+
+            });
+
             app.UseMvc();
+            
+        }
+
+        private void LogRequest(HttpRequest contextRequest)
+        {
+            var log = new StringBuilder();
+            log.AppendLine($"{contextRequest.Method} {contextRequest.GetDisplayUrl()}");
+            foreach (var header in contextRequest.Headers)
+            {
+                log.AppendLine($"{header.Key}: {header.Value}");
+            }
+
+            //var body = 
+            
+            Log.Info("middleware_" + log);
         }
     }
 }
