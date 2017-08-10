e44271c5384b44317f4236b220d6f2530703d9c9
Ivan Antsipau <ivan.antsipau@adform.com>
2017-08-10 22:21:41 +0300

    share random per controller

--- mqpi/Controllers/MockedController.cs
+++ mqpi/Controllers/MockedController.cs
@@ -7,12 +7,15 @@ namespace mqpi.Controllers
     [Route("api/{type}")]
     public class MockedController : Controller
     {
-        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(MockedController));
+        private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(typeof(MockedController));
+
+        private readonly Random _randomId = new Random(DateTime.UtcNow.Second);
+
         // GET api/values
         [HttpGet]
         public IEnumerable<dynamic> Get(string type)
         {
-            log.Info($"GET {type}");
+            Log.Info($"GET {type}");
             return new dynamic[]{};
         }
 
@@ -20,7 +23,7 @@ namespace mqpi.Controllers
         [HttpGet("{id}")]
         public ActionResult Get(string type, int id)
         {
-            log.Info($"GET {type} id = {id}");
+            Log.Info($"GET {type} id = {id}");
             return new NotFoundObjectResult($"{type} with id='{id}' was not found");
         }
 
@@ -28,8 +31,7 @@ namespace mqpi.Controllers
         [HttpPost]
         public object Post(string type, [FromBody]dynamic value)
         {
-            var random = new Random(DateTime.UtcNow.Second);
-            value.Id = random.Next();
+            value.Id = _randomId.Next();
 
             return value;
         }
