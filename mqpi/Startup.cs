af17566077e1cfd15eacaab7088c084d1ddcc410
Ivan Antsipau <ivan.antsipau@adform.com>
2017-08-12 23:19:01 +0300

    extract tracing into separate class

--- mqpi/Startup.cs
+++ mqpi/Startup.cs
@@ -1,24 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Extensions;
-using Microsoft.AspNetCore.Http.Internal;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace mqpi
 {
     public class Startup
     {
-        private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(typeof(Startup));
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -29,35 +16,8 @@ namespace mqpi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.Use(async (context, next) =>
-            {
-                LogRequest(context.Request);
-                await next.Invoke();
-            });
-
+            app.UseTracing();
             app.UseMvc();
-        }
-
-        private static void LogRequest(HttpRequest rq)
-        {
-            var log = new StringBuilder();
-            log.AppendLine($"{rq.Method} {rq.GetDisplayUrl()}");
-            foreach (var header in rq.Headers)
-            {
-                log.AppendLine($"{header.Key}: {header.Value}");
-            }
-
-            rq.EnableRewind();
-            // todo: check what happens if reader is finalized.
-            var reader = new StreamReader(rq.Body);
-            var content = reader.ReadToEnd();
-            rq.Body.Seek(0, SeekOrigin.Begin);
-            if (!string.IsNullOrEmpty(content))
-            {
-                log.AppendLine(content);
-            }
-
-            Log.Info(log);
         }
     }
 }
---
+++ mqpi/TracingMiddleware.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Http.Internal;
+
+namespace mqpi
+{
+    public class TracingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(typeof(TracingMiddleware));
+
+
+        public TracingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            LogRequest(context.Request);
+
+            // Call the next delegate/middleware in the pipeline
+            return _next(context);
+        }
+
+        private static void LogRequest(HttpRequest rq)
+        {
+            var log = new StringBuilder();
+            log.AppendLine($"{rq.Method} {rq.GetDisplayUrl()}");
+            foreach (var header in rq.Headers)
+            {
+                log.AppendLine($"{header.Key}: {header.Value}");
+            }
+
+            rq.EnableRewind();
+            // todo: check what happens if reader is finalized.
+            var reader = new StreamReader(rq.Body);
+            var content = reader.ReadToEnd();
+            rq.Body.Seek(0, SeekOrigin.Begin);
+            if (!string.IsNullOrEmpty(content))
+            {
+                log.AppendLine(content);
+            }
+
+            Log.Info(log);
+        }
+    }
+
+    public static class TracingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseTracing(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<TracingMiddleware>();
+        }
+    }
+}
