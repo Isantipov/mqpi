e5bc1787ba3419078d9c15ee7e0ebc402c87e746
Ivan Antsipau <ivan.antsipau@adform.com>
2017-10-07 03:45:56 +0300

    read json file

--- mqpi/Controllers/TracesController.cs
+++ mqpi/Controllers/TracesController.cs
@@ -3,26 +3,58 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using IOFile = System.IO.File;
 
 namespace mqpi.Controllers
 {
-    [Route("_sys/Traces")]
+    [Route("_sys/traces")]
     public class TracesController : Controller
     {
         public string Get(int offset = 0, int limit = 100)
         {
-            
             var lines = ReadLines()
                 .Skip(offset).Take(limit);
             var result = string.Join(Environment.NewLine, lines);
-            
+
             return result;
+        }
+
+        [Route("json")]
+        public ActionResult GetJson(int offset = 0, int limit = 100)
+        {
+            var res = OpenJsonFile();
+            return Ok(res);
+        }
+
+        private IEnumerable<TraceItem> OpenJsonFile()
+        {
+            using (FileStream file = IOFile.Open("/var/log/mqpi/json_traces.log", FileMode.OpenOrCreate,
+                FileAccess.Read, FileShare.ReadWrite))
+            using (StreamReader streamReader = new StreamReader(file))
+            using (JsonTextReader reader = new JsonTextReader(streamReader))
+            {
+                reader.SupportMultipleContent = true;
+
+
+                var serializer = new JsonSerializer();
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader.LineNumber}:{reader.LinePosition}");
+
+                    if (reader.TokenType == JsonToken.StartObject)
+                    {
+                        var c = serializer.Deserialize<TraceItem>(reader);
+                        yield return c;
+                    }
+                }
+            }
         }
 
         private static IEnumerable<string> ReadLines()
         {
-            using (FileStream file = IOFile.Open("/var/log/mqpi/text.log", FileMode.OpenOrCreate, FileAccess.Read, FileShare.ReadWrite))
+            using (FileStream file = IOFile.Open("/var/log/mqpi/text.log", FileMode.OpenOrCreate, FileAccess.Read,
+                FileShare.ReadWrite))
             {
                 using (var textReader = new StreamReader(file))
                 {
